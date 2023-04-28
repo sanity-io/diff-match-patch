6782a42473e2075c351dd4c9d5cf3bfd00881623
Espen Hovlandsdal <espen@hovlandsdal.com>
2023-04-27 19:24:19 -0700

    refactor: move `addPadding` to its own file

--- src/patch/__tests__/addPadding.test.ts
+++ src/patch/__tests__/addPadding.test.ts
@@ -1,24 +1,27 @@
-import {test, expect} from 'vitest'
-import {addPadding} from '../apply.js'
+import {describe, test, expect} from 'vitest'
+import {addPadding} from '../addPadding.js'
 import {make} from '../make.js'
 import {stringify} from '../stringify.js'
 
-test('addPadding', () => {
-  // Both edges full.
-  let patches = make('', 'test')
-  expect(stringify(patches)).toBe('@@ -0,0 +1,4 @@\n+test\n')
-  addPadding(patches)
-  expect(stringify(patches)).toBe('@@ -1,8 +1,12 @@\n %01%02%03%04\n+test\n %01%02%03%04\n')
+describe('addPadding', () => {
+  test('Both edges full', () => {
+    const patches = make('', 'test')
+    expect(stringify(patches)).toBe('@@ -0,0 +1,4 @@\n+test\n')
+    addPadding(patches)
+    expect(stringify(patches)).toBe('@@ -1,8 +1,12 @@\n %01%02%03%04\n+test\n %01%02%03%04\n')
+  })
 
-  // Both edges partial.
-  patches = make('XY', 'XtestY')
-  expect(stringify(patches)).toBe('@@ -1,2 +1,6 @@\n X\n+test\n Y\n')
-  addPadding(patches)
-  expect(stringify(patches)).toBe('@@ -2,8 +2,12 @@\n %02%03%04X\n+test\n Y%01%02%03\n')
+  test('Both edges partial', () => {
+    const patches = make('XY', 'XtestY')
+    expect(stringify(patches)).toBe('@@ -1,2 +1,6 @@\n X\n+test\n Y\n')
+    addPadding(patches)
+    expect(stringify(patches)).toBe('@@ -2,8 +2,12 @@\n %02%03%04X\n+test\n Y%01%02%03\n')
+  })
 
-  // Both edges none.
-  patches = make('XXXXYYYY', 'XXXXtestYYYY')
-  expect(stringify(patches)).toBe('@@ -1,8 +1,12 @@\n XXXX\n+test\n YYYY\n')
-  addPadding(patches)
-  expect(stringify(patches)).toBe('@@ -5,8 +5,12 @@\n XXXX\n+test\n YYYY\n')
+  test('Both edges none', () => {
+    const patches = make('XXXXYYYY', 'XXXXtestYYYY')
+    expect(stringify(patches)).toBe('@@ -1,8 +1,12 @@\n XXXX\n+test\n YYYY\n')
+    addPadding(patches)
+    expect(stringify(patches)).toBe('@@ -5,8 +5,12 @@\n XXXX\n+test\n YYYY\n')
+  })
 })
---
+++ src/patch/addPadding.ts
@@ -0,0 +1,64 @@
+import {DIFF_EQUAL} from '../diff/diff.js'
+import {DEFAULT_MARGIN} from './constants.js'
+import type {Patch} from './createPatchObject.js'
+
+/**
+ * Add some padding on text start and end so that edges can match something.
+ * Intended to be called only from within patch_apply.
+ *
+ * @param patches - Array of Patch objects.
+ * @param margin - The margin length to add.
+ * @returns The string, padded on each side.
+ * @internal
+ */
+export function addPadding(patches: Patch[], margin: number = DEFAULT_MARGIN): string {
+  const paddingLength = margin
+  let nullPadding = ''
+  for (let x = 1; x <= paddingLength; x++) {
+    nullPadding += String.fromCharCode(x)
+  }
+
+  // Bump all the patches forward.
+  for (const p of patches) {
+    p.start1 += paddingLength
+    p.start2 += paddingLength
+  }
+
+  // Add some padding on start of first diff.
+  let patch = patches[0]
+  let diffs = patch.diffs
+  if (diffs.length === 0 || diffs[0][0] !== DIFF_EQUAL) {
+    // Add nullPadding equality.
+    diffs.unshift([DIFF_EQUAL, nullPadding])
+    patch.start1 -= paddingLength // Should be 0.
+    patch.start2 -= paddingLength // Should be 0.
+    patch.length1 += paddingLength
+    patch.length2 += paddingLength
+  } else if (paddingLength > diffs[0][1].length) {
+    // Grow first equality.
+    const extraLength = paddingLength - diffs[0][1].length
+    diffs[0][1] = nullPadding.substring(diffs[0][1].length) + diffs[0][1]
+    patch.start1 -= extraLength
+    patch.start2 -= extraLength
+    patch.length1 += extraLength
+    patch.length2 += extraLength
+  }
+
+  // Add some padding on end of last diff.
+  patch = patches[patches.length - 1]
+  diffs = patch.diffs
+  if (diffs.length === 0 || diffs[diffs.length - 1][0] !== DIFF_EQUAL) {
+    // Add nullPadding equality.
+    diffs.push([DIFF_EQUAL, nullPadding])
+    patch.length1 += paddingLength
+    patch.length2 += paddingLength
+  } else if (paddingLength > diffs[diffs.length - 1][1].length) {
+    // Grow last equality.
+    const extraLength = paddingLength - diffs[diffs.length - 1][1].length
+    diffs[diffs.length - 1][1] += nullPadding.substring(0, extraLength)
+    patch.length1 += extraLength
+    patch.length2 += extraLength
+  }
+
+  return nullPadding
+}
